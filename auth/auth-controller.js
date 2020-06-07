ffcb26b19c6543308e66b3b505fd7a042ddea83f
mnai01 <imatlak03@gmail.com>
2020-06-06 21:59:24 -0400

    dotenv require added

--- auth/auth-controller.js
+++ auth/auth-controller.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const User = require('../models/user.js');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
@@ -42,51 +43,59 @@ app.post ("/logout", async(req, res) => {
 
 })*/
 module.exports.getUsers = async (req, res) => {
-	try {
-		const result = await User.findAll()
-		res.status(200).json({users: result})
-	} catch(err) {
-		console.error(err);
-		res.status(500).json({message: 'Query failed'})
-	}
-} 
+  try {
+    const result = await User.findAll();
+    res.status(200).json({ users: result });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Query failed' });
+  }
+};
 
 module.exports.signUp = async (req, res) => {
-	try {
-		const hash =  await bcrypt.hash(req.body.password, 10);
-		const data = {
-			username: req.body.username,
-			password: hash,
-			first_name: req.body.first_name,
-			last_name: req.body.last_name,
-			dob: req.body.dob
-		}
-		await User.create(data);
-		res.status(200).json({'message': 'User created'});
-	} catch(err) {
-		console.log(err); 
-		res.status(500).json({'message': 'Registration failed'})
-	}
-}
+  try {
+    const hash = await bcrypt.hash(req.body.password, 10);
+    const data = {
+      username: req.body.username,
+      password: hash,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      dob: req.body.dob,
+    };
+    await User.create(data);
+    res.status(200).json({ message: 'User created' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Registration failed' });
+  }
+};
 
 module.exports.logIn = async (req, res) => {
-	try { 
-    	const result = await User.findOne({where: {username: req.body.username}});
-    	const user = result.dataValues;
-    	if(result == null) {
-    		res.status(500).json({'message': 'Auth failed'})
-    	} else {
-    		const cmpResult = await bcrypt.compare(req.body.password, user.password);
-    		if(cmpResult) {
-    			const payLoad = {userId: user.user_id, username: user.username};
-    			const options = {algorithm: 'HS256', expiresIn: '1h'};
-    			const token = jwt.sign(payLoad, process.env.ACCESS_TOKEN_SECRET, options);
-				return res.status(200).json({message: 'Auth successful', token: token})
-    		} else {
-    			res.status(401).json({'message': 'Auth failed'})
-    		}
-    	}
-    } catch {
-    	res.status(500).json({'message': 'Auth failed'})
+  try {
+    const result = await User.findOne({
+      where: { username: req.body.username },
+    });
+    const user = result.dataValues;
+    if (result == null) {
+      res.status(500).json({ message: 'Auth failed' });
+    } else {
+      const cmpResult = await bcrypt.compare(req.body.password, user.password);
+      if (cmpResult) {
+        const payLoad = { userId: user.user_id, username: user.username };
+        const options = { algorithm: 'HS256', expiresIn: '1h' };
+        const token = jwt.sign(
+          payLoad,
+          process.env.ACCESS_TOKEN_SECRET,
+          options
+        );
+        return res
+          .status(200)
+          .json({ message: 'Auth successful', token: token });
+      } else {
+        res.status(401).json({ message: 'Auth failed' });
+      }
     }
-}
+  } catch {
+    res.status(500).json({ message: 'Auth failed' });
+  }
+};
